088a181c14ec9ba8ba3073ca4bfca83581e1c260
yanghuan <sy.yanghuan@gmail.com>
2020-05-14 19:42:53 +0800

    update

--- runtime/outcpp/test.cpp
+++ runtime/outcpp/test.cpp
@@ -1,11 +1,12 @@
-#include <System.Private.CoreLib/System/Array.h>
+//#include <System.Private.CoreLib/System/Array.h>
 #include <System.Private.CoreLib/System/String.h>
 
 using namespace System;
 
 int main()
 {
-  array<String> arr = *(array<String>*)&newarr<String>(10);
-  arr[0] = "dddd";
+  newarr<String>(10);
+  //array<String> arr = *(array<String>*)&newarr<String>(10);
+  //arr[0] = "dddd";
   return 0;
 }
