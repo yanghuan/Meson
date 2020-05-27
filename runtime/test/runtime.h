3b11f774675ac3b53c6a268d1b4c7dedc75434df
yanghuan <sy.yanghuan@gmail.com>
2020-05-27 14:14:18 +0800

    updtae

--- runtime/test/runtime.h
+++ runtime/test/runtime.h
@@ -1,12 +1,2 @@
 ﻿#pragma once
 
-#if 0
-namespace System::Collections {
-  class IComparer {};
-}
-
-namespace System {
-  using namespace System::Collections;
-}
-#endif
-
--- runtime/test/test.cpp
+++ runtime/test/test.cpp
@@ -1,6 +1,5 @@
 #include "runtime.h"
 
-
 int main() {
   
   return 0;
