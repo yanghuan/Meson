78425f08e16ccdd1b8b32d120f6410959f4eaea5
YANG Huan <sy.yanghuan@gmail.com>
2020-05-29 00:52:08 +0800

    gx

--- runtime/xmake.lua
+++ runtime/xmake.lua
@@ -2,10 +2,12 @@ set_project("runtime")
 set_languages("c++20")
 add_requires("boost")
 
+add_rules("mode.debug", "mode.release")
+
 if is_mode("debug") then 
   add_defines("DEBUG")
-  set_symbols("debug")
-  set_optimize("none")
+  --set_symbols("debug")
+  --set_optimize("none")
 end
 
 includes("outcpp")
