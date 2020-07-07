1e8d048eda1016eff8cef499de61b86dcc70be4c
YANG Huan <sy.yanghuan@gmail.com>
2020-07-07 23:38:34 +0800

    gx

--- runtime/outcpp/xmake.lua
+++ runtime/outcpp/xmake.lua
@@ -1,7 +1,69 @@
-rule("cxxsha1")
+rule("cxxfilehash")
   set_extensions(".cpp")
   before_load(function (target)
-    target:data_set("cxxsha1.files_hash", {})
+    local hashscache = {}
+
+    local function getfiledata(file)
+      local data = hashscache[file] or {}
+      hashscache[file] = data
+      return data
+    end
+
+    local function getfilesize(file)
+      local data = getfiledata(file)
+      local size = data[1] or os.filesize(file)
+      data[1] = size
+      return size
+    end
+
+    local function filehash(file)
+      return try {
+        function ()
+          return hash.sha256(file)
+        end
+      }
+    end
+
+    local function getfilehash(file)
+      local data = getfiledata(file)
+      local hashcode = data[2] or filehash(file)
+      data[2] = hashcode
+      return hashcode
+    end
+
+    local function ishashchanged(dependinfo)
+      local hashs = dependinfo.hashs or {}
+      for _, file in ipairs(dependinfo.files) do
+        local data = hashs[file] or {}
+        local size = getfilesize(file)
+        if data[1] ~= size then
+          print(file, data[1], size)
+          return true
+        end
+        local hashcode = getfilehash(file)
+        if hashcode and data[2] ~= hashcode then
+          print(file, data[2], hashcode)
+          return true
+        end
+      end
+      return false
+    end
+
+    local function updatehash(dependinfo)
+      local hashs = {}
+      dependinfo.hashs = hashs
+      for _, file in ipairs(dependinfo.files) do
+        hashs[file] = {
+          getfilesize(file),
+          getfilehash(file)
+        }
+      end
+    end
+
+    target:data_set("cxxfilehash.filehash", {
+      ishashchanged = ishashchanged,
+      updatehash = updatehash,
+    })
   end)
   on_build_file(function (target, sourcefile, opt)
     import("core.base.option")
@@ -9,9 +71,9 @@ rule("cxxsha1")
     import("core.project.depend")
     import("core.tool.compiler")
     import("private.utils.progress")
-    import("cxxsha1")
 
-    local files_hash = target:data("cxxsha1.files_hash")
+    local filehash = target:data("cxxfilehash.filehash")
+
     local objectfile = target:objectfile(sourcefile)
     local compinst = compiler.load("cxx", {target = target})
     local compflags = compinst:compflags({target = target, sourcefile = sourcefile})
@@ -21,7 +83,7 @@ rule("cxxsha1")
     local dependinfo = option.get("rebuild") and {} or (depend.load(dependfile) or {})
     local depvalues = {compinst:program(), compflags}
     if depend.is_changed(dependinfo, {lastmtime = 9223372036854775807, values = depvalues}) then
-    elseif cxxsha1.is_hash_changed(dependinfo, files_hash) then
+    elseif filehash.ishashchanged(dependinfo) then
     else
       return
     end
@@ -29,14 +91,15 @@ rule("cxxsha1")
     progress.show(opt.progress, "compiling.%s %s", config.get("mode"), sourcefile)
     assert(compinst:compile(sourcefile, objectfile, {dependinfo = dependinfo, compflags = compflags}))
     dependinfo.values = depvalues
-    cxxsha1.update_hash(dependinfo, files_hash)
+    filehash.updatehash(dependinfo)
     depend.save(dependinfo, dependfile)
   end)
   
 target("outcpp")
   set_kind("binary")
-  add_rules("cxxsha1")
+  add_rules("cxxfilehash")
   add_packages("boost")
   add_includedirs("..", {public = true})
   add_includedirs(".")
-  add_files("../rt/**.cpp", "**.cpp", { rules = { "cxxsha1", override = true }})
+  add_files("../rt/**.cpp", "**.cpp", { rules = { "cxxfilehash", override = true }})
+
